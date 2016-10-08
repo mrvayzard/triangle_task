346876aa9f267da78015522b786ab72786252d8d
mrvayzard <mrvayzard@ukr.net>
2016-10-08 12:43:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            List<int> prime = new List<int>();
+            prime.Add(2);
+            for (int i = 3; i < 1000000; i++)
+            {
+                string number = i.ToString();
+                for (int j=0; j<number.Length; j++)
+                {
+                    int num = Convert.ToInt32(number);
+                    for (int k=0; k<prime.Count; k++)
+                    {
+                        if (num % prime[k] == 0 && num!= prime[k])
+                        {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    char temp = number[0];
+                    number = number.Remove(0, 1);
+                    number += temp;
+                }
+                if (flag == false)
+                    prime.Add(i);
+                flag = false;
+            }
+        }
+    }
+}
