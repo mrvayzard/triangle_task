ce23f0698c75c392d5282b9fe694a8c568a0248e
Denys Yablonskyy <mrvayzard@ukr.net>
2016-10-08 14:59:59 +0300

    Update Ex1.cs

--- Ex1.cs
+++ Ex1.cs
@@ -1,54 +1,112 @@
-/* Creating by Yablonskyy Denys <mrvayzard@ukr.net> */
-
 using System;
 using System.Collections.Generic;
 
-class Ex1
+class Program
 {
-    static List<int> FindCircularPrimeNumbers(int limit)    //finding circular prime numbers
+    public struct edge
     {
-        bool flag = false;
-        List<int> prime = new List<int>(); //list of circular prime numbers
-        prime.Add(2);
-        for (int i = 3; i < limit; i++)
+        public int a, b, cost;
+
+        public edge(int a, int b, int cost)
         {
-            string number = i.ToString();
-            for (int j = 0; j < number.Length; j++)
-            {
-                int num = Convert.ToInt32(number);
-                for (int k = 0; k < prime.Count; k++)
-                {
-                    if (num % prime[k] == 0 && num != prime[k]) //if number not circular prime
-                    {
-                        flag = true;
-                        break;
-                    }
-                }
-                //next 3 line is number discharge shift (to left)
-                char temp = number[0];
-                number = number.Remove(0, 1);
-                number += temp;
-            }
-            if (flag == false)  //if number is circular prime
-                prime.Add(i);
-            flag = false;
+            this.a = a;
+            this.b = b;
+            this.cost = cost;
         }
-        return prime;   //return list of circular prime numbers
     }
 
     static void Main(string[] args)
     {
-        int limit = 1000000;    //max number value
-        List <int> circularPrimeNumbers = FindCircularPrimeNumbers(limit);  //list of circular prime numbers
-        Console.WriteLine("Count of cycles prime numbers: " + circularPrimeNumbers.Count);  //output count ofcircular prime numbers to console
 
-        /*Uncomment to output cycles prime numbers*/
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        // the code that you want to measure comes here
 
-        //foreach (var item in cyclesPrimeNumbers)
-        //{
-        //    Console.Write(item + " ");
-        //}
+        int countVertexes = 0;
+        List<int> weight = new List<int> { };
+        List<edge> ed = new List<edge> { };
 
+        //read data from file
+
+        try
+        {
+            using (StreamReader sr = new StreamReader("Test.txt"))
+            {
+                String line = null;
+                line = sr.ReadToEnd();
+                string[] rowNums = line.Split();
+                for (int i = 0; i < rowNums.Length; i++)
+                {
+                    if(rowNums[i]!="")
+                        weight.Add(Convert.ToInt32(rowNums[i]));
+                }
+                countVertexes = weight.Count;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
+        //creating edges data
+
+        int[,] newMatrix = new int [countVertexes, countVertexes];
+
+        int rowCount = 0;
+        int endValue = 0;
+        int zeroCount = 0;
+        int zeroCount2 = 0;
+        for (int i = 0; i < countVertexes; i++)
+        {
+            zeroCount2=0;
+            for (int j = zeroCount; j < countVertexes; j++)
+            {
+                zeroCount2++;
+                if (rowCount != 0 || j != zeroCount && zeroCount2 <= 3)
+                {
+                    edge smth = new edge(i, j, weight[j]);
+                    ed.Add(smth);
+                }
+            }
+
+            if (rowCount == endValue)
+            {
+                zeroCount += 2;
+                rowCount = 0;
+                endValue++;
+            }
+            else
+            {
+                zeroCount++;
+                rowCount++;
+            }
+        }
+
+        //Belman-Ford algorithm
+
+        int[] d = new int[countVertexes];
+
+        for (int i = 0; i < countVertexes; i++)
+        {
+            d[i] = -999999999;
+        }
+
+        d[0] = 0;
+
+        for (int i = 0; i < countVertexes; i++)
+        {
+            for (int k = 0; k < ed.Count; k++)
+            {
+                if (d[ed[k].b] < d[ed[k].a] + ed[k].cost)
+                    d[ed[k].b] = d[ed[k].a] + ed[k].cost;
+            }
+        }
+
+        Console.WriteLine("Max way: " + (d.Max() + weight[0]));
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+        Console.WriteLine(elapsedMs);
         Console.Read();
     }
+
 }
